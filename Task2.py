47af6b9fd59514a3ecc495030faf1305cb2e8d35
Shalini Singh <151626296+shalushailendrasingh@users.noreply.github.com>
2025-09-28 18:42:55 +0530

    Add files via upload
    Problem Statement :-
    Write a Python program that:
    1. Creates a list of numbers from 1 to 10.
    2. Extracts the first five elements from the list.
    3. Reverses these extracted elements.
    4. Prints both the extracted list and the reversed list.

---
+++ Task2.py
@@ -0,0 +1,10 @@
+
+numbers = list(range(1, 11))
+
+first_five = numbers[:5]
+
+reversed_list = first_five[::-1]
+
+# Step 4: Print both lists
+print("Original List:", first_five)
+print("Reversed List:", reversed_list)
